ab05b329632a6de79c5340cb54658d30351edcb3
torupa <xpt77@outlook.com>
2025-02-07 22:39:16 +0900

    fix conflict

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
-FROM python:3.9-slim-buster
+FROM python:3.12-slim
 
 WORKDIR /app
 
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 
-COPY . .
+COPY app.py .
+
+EXPOSE 8501
 
 CMD ["streamlit", "run", "app.py"]
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.title("Hello Streamlit!")
-
+st.title("Hello World!")
+st.write("This is a Streamlit app running in Docker.")
 st.write("This is a sample Streamlit app running on Cloud Run.")
 
 x = st.slider("Select a value", 0, 100)
